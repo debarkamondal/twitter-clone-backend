5e753b24641bf4a31ca66197cb5c24ad103aca52
debarka <debarkamondal@gmail.com>
2023-09-13 03:40:47 +0530

    test

--- src/app/services/tweet.ts
+++ src/app/services/tweet.ts
@@ -25,8 +25,8 @@ export class TweetService {
 		return tweet;
 	}
 	public static async getAllTweets() {
-		const cachedTweets = await redisClient.get("ALL_TWEETS");
-		if (cachedTweets) return JSON.parse(cachedTweets);
+		// const cachedTweets = await redisClient.get("ALL_TWEETS");
+		// if (cachedTweets) return JSON.parse(cachedTweets);
 		const tweets = await prismaClient.tweet.findMany({
 			orderBy: { createdAt: "desc" },
 		});
